dcaa382a43676882805b679ec85087f2535e600f
Davyd_Rybak <Davyd_Rybak@epam.com>
2017-11-07 18:03:28 +0200

    init commit

---
+++ src/main/groovy/com/galley/larder/LarderApplication.groovy
@@ -0,0 +1,12 @@
+package com.galley.larder
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class LarderApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run LarderApplication, args
+	}
+}
