e91bb19ae8b2a1767da36a4e716bf2aa2d801930
Davyd_Rybak <Davyd_Rybak@epam.com>
2017-11-13 20:08:54 +0200

    some experiments with graphql

---
+++ src/main/java/com/galley/larder/api/graphql/resolver/Query.java
@@ -0,0 +1,8 @@
+package com.galley.larder.api.graphql.resolver;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Query implements GraphQLQueryResolver {
+}
