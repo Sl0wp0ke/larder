e10c323afaf4b97cd4fc1f79be42ebcff90448e9
Davyd_Rybak <Davyd_Rybak@epam.com>
2017-11-20 20:10:53 +0200

    added config

--- src/main/java/com/galley/larder/api/graphql/resolver/Query.java
+++ src/main/java/com/galley/larder/api/graphql/resolver/Query.java
@@ -1,8 +1,15 @@
 package com.galley.larder.api.graphql.resolver;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.galley.larder.model.User;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class Query implements GraphQLQueryResolver {
+    public List<User> allUser() {
+        return new ArrayList<User>();
+    }
 }
---
+++ src/main/java/com/galley/larder/model/User.java
@@ -0,0 +1,11 @@
+package com.galley.larder.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private int id;
+    private String name;
+    private String lastName;
+    private boolean sex;
+}
