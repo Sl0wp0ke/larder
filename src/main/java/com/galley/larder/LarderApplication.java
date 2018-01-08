046dee5d279b5985fb0832cdea5bf0b0a85886b4
snark <nord.slack@gmail.com>
2018-01-08 17:32:42 +0200

    Remove security

--- src/main/java/com/galley/larder/LarderApplication.java
+++ src/main/java/com/galley/larder/LarderApplication.java
@@ -16,27 +16,7 @@ import java.util.Map;
 import java.util.UUID;
 
 @SpringBootApplication
-@Controller
 public class LarderApplication {
-    @GetMapping("/user")
-    @ResponseBody
-    public Principal user(Principal user) {
-        return user;
-    }
-
-    @GetMapping("/resource")
-    @ResponseBody
-    public Map<String, Object> home() {
-        Map<String, Object> model = new HashMap<String, Object>();
-        model.put("id", UUID.randomUUID().toString());
-        model.put("content", "Hello World");
-        return model;
-    }
-
-    @GetMapping(value = "/{path:[^\\.]*}")
-    public String redirect() {
-        return "forward:/";
-    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(LarderApplication.class, args);
