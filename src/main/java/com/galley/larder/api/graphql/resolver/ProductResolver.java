d80a43339fa8b359d5c350cd644fe90121b2c578
snark <nord.slack@gmail.com>
2018-01-22 00:27:35 +0200

    crap with modules

--- src/main/java/com/galley/larder/api/graphql/resolver/ProductResolver.java
+++ src/main/java/com/galley/larder/api/graphql/resolver/ProductResolver.java
@@ -1,18 +1,45 @@
 package com.galley.larder.api.graphql.resolver;
 
-//import com.coxautodev.graphql.tools.GraphQLRootResolver;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.galley.larder.excaptions.ProductNotFoundException;
 import com.galley.larder.models.Product;
+import jdk.incubator.http.HttpClient;
+import jdk.incubator.http.HttpRequest;
+import jdk.incubator.http.HttpResponse;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 @Component
-//public class Query implements GraphQLRootResolver {
 public class ProductResolver implements GraphQLQueryResolver {
     public Product getProdact(long barcode) {
-//        Product product = new Product();
-//        product.setName("loshara");
-//        return product;
+        HttpClient httpClient = HttpClient.newHttpClient();
+
+        URI uri = null;
+        try {
+            uri = new URI("http://ean13.info/4820017000277.htm");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(uri)
+                .GET()
+                .build();
+
+        try {
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandler.asString());
+            Product product = new Product();
+            product.setName(response.body());
+            return product;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
         throw new ProductNotFoundException();
     }
 }
---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module larder.main {
+    requires graphql.java;
+    requires jdk.incubator.httpclient;
+    requires graphql.java.tools;
+    requires spring.context;
+    requires graphql.java.servlet;
+    requires spring.web;
+    requires spring.beans;
+    requires lombok;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+}
